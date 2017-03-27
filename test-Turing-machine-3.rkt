d3fefd3961175a1c7c7afb69733335787023e1e1
Jos Koot <joskoot@users.noreply.github.com>
2017-03-27 16:19:35 +0200

    Add files via upload

--- test-Turing-machine-3.rkt
+++ test-Turing-machine-3.rkt
@@ -5,9 +5,10 @@
 ;===================================================================================================
 
 (define rules
-'(((0 B) (T _ N))
-  ((0 y) (F _ N))
-  ((0 _) (0 _ R))))
+'(((0 b) (F _ N))
+  ((0 B) (T _ N))
+  ((0 _) (0 _ R))
+  ((1 B) (T _ L))))
 
 (define Turing-machine (make-Turing-machine '0 '(T F) 'B 'b '_ rules))
 
@@ -15,7 +16,7 @@
 
 (test Turing-machine '()                'T '())
 (test Turing-machine '(p q r s t)       'T '(p q r s t))
-(test Turing-machine '(x x y x x)       'F '(x x y x x))
+(test Turing-machine '(x x b x x)       'F '(x x b x x))
 
 (test-report)
 
